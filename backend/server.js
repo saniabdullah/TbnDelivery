5efa19bb678bad24f6a5e9b5a4f3243e5fc8f2ab
saniabdullah <abdullah.s@students.amikom.ac.id>
2022-09-15 11:23:19 +0800

    first commit

---
+++ backend/server.js
@@ -0,0 +1,50 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const adminRouter = require('./routes/admin')
+const productRoutes = require('./routes/products')
+const cors = require('cors')
+
+
+// express app
+const app = express()
+
+// cors
+app.use(cors())
+
+// middleware
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+app.use('/api/products', productRoutes)
+app.use('/api/admin', adminRouter)
+
+// deploy
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.get("*", function(_, res) {
+  res.sendFile(
+    path.join(__dirname, "../frontend/build/index.html"),
+    function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    }
+  )
+})
+
+// connect to db
+mongoose.connect(process.env.MONG_URI)
+  .then(() => {
+    console.log('connected to database')
+    // listen to port
+    app.listen(process.env.PORT, () => {
+      console.log('listening for requests on port', process.env.PORT)
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+  }) 
